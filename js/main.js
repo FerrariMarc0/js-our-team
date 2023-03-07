d1bd8e0cc815ed2550276d5b51940ec504e41772
FerrariMarc0 <marco1010@live.it>
2023-03-07 17:30:00 +0100

    img

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@
 
 const team= [  
 
-    {nome: 'Wayne Barnett', ruolo: 'Founder & CEO', img: 'wayne-barnett-founder-ceo.jpg'},
-    {nome: 'Angela Caroll', ruolo: 'Chief Editor', img: '<img src="img/angela-caroll-chief-editor.jpg" alt="angela"></img>'},
-    {nome: 'Walter Gordon', ruolo: 'Office Manager', img: 'walter-gordon-office-manager.jpg'},
-    {nome: 'Angela Lopez', ruolo: 'Social Media Manager', img: 'angela-lopez-social-media-manager.jpg'},
-    {nome: 'Scott Estrada', ruolo: 'Developer', img: 'scott-estrada-developer.jpg'},
+    {nome: 'Wayne Barnett', ruolo: 'Founder & CEO', img: '<img src="img/wayne-barnett-founder-ceo.jpg" alt="wayne"></img>'},
+    {nome: 'Angela Caroll', ruolo: 'Chief Editor', img: '<img src="img/angela-caroll-chief-editor.jpg" alt="caroll"></img>'},
+    {nome: 'Walter Gordon', ruolo: 'Office Manager', img: '<img src="img/walter-gordon-office-manager.jpg" alt="walter"></img>'},
+    {nome: 'Angela Lopez', ruolo: 'Social Media Manager', img: '<img src="img/angela-lopez-social-media-manager.jpg" alt="angela"></img>'},
+    {nome: 'Scott Estrada', ruolo: 'Developer', img: '<img src="img/scott-estrada-developer.jpg" alt="scott"></img>'},
 ];
-team.push({nome: 'Barbara Ramos', ruolo: 'Graphic Designer', img: 'barbara-ramos-graphic-designer.jpg'});
+team.push({nome: 'Barbara Ramos', ruolo: 'Graphic Designer', img: '<img src="img/barbara-ramos-graphic-designer.jpg" alt="barbara"></img>'});
 
 console.log(team);
 
