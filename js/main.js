9ecc82b0a020359029b77077460c6b869abf4353
FerrariMarc0 <marco1010@live.it>
2023-03-07 15:35:58 +0100

    array object c_log

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const team= [  
+
+    {nome: 'Wayne Barnett', ruolo: 'Founder & CEO', img: 'wayne-barnett-founder-ceo.jpg'},
+    {nome: 'Angela Caroll', ruolo: 'Chief Editor', img: 'angela-caroll-chief-editor.jpg'},
+    {nome: 'Walter Gordon', ruolo: 'Office Manager', img: 'walter-gordon-office-manager.jpg'},
+    {nome: 'Angela Lopez', ruolo: 'Social Media Manager', img: 'angela-lopez-social-media-manager.jpg'},
+    {nome: 'Scott Estrada', ruolo: 'Developer', img: 'scott-estrada-developer.jpg'},
+];
+team.push({nome: 'Barbara Ramos', ruolo: 'Graphic Designer', img: 'barbara-ramos-graphic-designer.jpg'});
+
+console.log(team);
+
+for(let i = 0; i < team.length; i++){
+    console.log(team[i]);
+    console.log(team[i].nome);
+    console.log(team[i]['ruolo']);
+    console.log(team[i].img);
+}
+
